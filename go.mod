26b3543304cdbe2f036cf7eb601efa9c4a1929fc
Ali Shanaakh <github@shal.dev>
2019-07-14 18:40:21 +0300

    Improve code stability

--- go.mod
+++ go.mod
@@ -1,9 +1,11 @@
 module github.com/opencars/opencars
 
+go 1.12
+
 require (
 	github.com/go-pg/pg v8.0.4+incompatible
-	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
-	github.com/json-iterator/go v1.1.5
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.6
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
--- internal/http/http.go
+++ internal/http/http.go
@@ -4,13 +4,19 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
-	jsoniter "github.com/json-iterator/go"
+	"github.com/json-iterator/go"
 
 	"github.com/opencars/opencars/internal/database"
 	"github.com/opencars/opencars/pkg/model"
 	"github.com/opencars/opencars/pkg/translator"
 )
+
+// Error is error JSON format with error description.
+type Error struct {
+	Error string `json:"error"`
+}
 
 var (
 	// Storage is an instance of Database Interface.
@@ -18,31 +24,46 @@ var (
 	json    = jsoniter.ConfigFastest
 )
 
+func SendError(w http.ResponseWriter, code int, msg string) {
+	w.WriteHeader(code)
+
+	if err := json.NewEncoder(w).Encode(Error{msg}); err != nil {
+		log.Panic(err.Error())
+	}
+}
+
 func Transport(w http.ResponseWriter, req *http.Request) {
 	cars := make([]model.Operation, 0)
 	number := translator.ToUA(req.FormValue("number"))
 	limit := 1
 
+	w.Header().Set("Server", "opencars")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
 	if number == "" {
-		http.Error(w, "number is empty", http.StatusBadRequest)
+		SendError(w,http.StatusBadRequest, "number is empty")
 		return
 	}
 
 	if tmp, err := strconv.Atoi(req.FormValue("limit")); err == nil {
 		limit = tmp
+	} else if req.FormValue("limit") != "" {
+		SendError(w,http.StatusBadRequest, "limit is not valid")
+		return
 	}
 
 	if err := Storage.Select(&cars, limit, "number = ?", number); err != nil {
+		SendError(w,http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		log.Println(err)
-		http.Error(w, "", http.StatusInternalServerError)
+		return
 	}
 
 	if err := json.NewEncoder(w).Encode(cars); err != nil {
+		SendError(w,http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 		log.Println(err)
-		http.Error(w, "", http.StatusInternalServerError)
+		return
 	}
-
-	w.Header().Set("Content-Type", "application/json")
 }
 
 // HealthHandler is a net/http handler for health checks.
@@ -54,12 +75,23 @@ func Health(w http.ResponseWriter, _ *http.Request) {
 }
 
 func Run(addr string) {
-	http.HandleFunc("/transport", Transport)
-	http.HandleFunc("/health", Health)
-
 	log.Printf("Server is listening %s\n", addr)
 
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		panic(err.Error())
+	router := http.NewServeMux()
+	router.HandleFunc("/transport", Transport)
+	router.HandleFunc("/health", Health)
+
+	server := &http.Server{
+		Addr:         addr,
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	server.SetKeepAlivesEnabled(true)
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Could not listen on %s. Error: %v\n", addr, err)
 	}
 }
